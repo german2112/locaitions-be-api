c0bcd04908f016112d692c12165533dc1dd34a64
german211221 <german211221@gmail.com>
2023-07-17 20:10:09 -0600

    chore: fix user schema and uid integration

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ async def root():
  database.db["Users"].find()
  return database.db["Users"].find()
 
-def find_user_by_phone(phone: int):
+def find_user_by_phone(phone: str):
  user_exists = database.db["Users"].find_one({"phone": phone})
  return user_exists
 
@@ -19,10 +19,15 @@ def find_user_by_email(email: str):
  user_exists = database.db["Users"].find_one({"email": email})
  return user_exists
 
-@app.post("/create-user", response_description="Create new user", response_model=UserSchema)
+def find_user_by_id(uid: str):
+  user_exists = database.db["Users"].find_one({"uid": uid})
+  return user_exists
+
+@app.post("/initialize-user", response_description="Create new user", response_model=UserSchema, tags=["users"])
 def create_membership(user: UserSchema = Body(...)):
-    if find_user_by_email(user.email) or find_user_by_phone(user.phone): 
+    if find_user_by_email(user.email) or find_user_by_phone(user.phone) or find_user_by_id(user.uid): 
       return JSONResponse(status_code=status.HTTP_409_CONFLICT, content={"message": "User already exists"})
     user = jsonable_encoder(user)
-    database.db["Users"].insert_one(user)  
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content={"message": "User created"})
+    created_user = database.db["Users"].insert_one(user)
+    response: str = "user created with id"+created_user.inserted_id
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content={"message": response})

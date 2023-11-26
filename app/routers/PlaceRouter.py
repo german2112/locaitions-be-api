d2a5696d50693c44a81ce4346063eb59cbe3e39d
german2112 <super.g.osi@hotmail.com>
2023-11-26 14:48:18 -0600

    feature: pagination of places

--- app/routers/PlaceRouter.py
+++ app/routers/PlaceRouter.py
@@ -4,14 +4,18 @@ from app.models.Place import PlaceSchema
 from app.models.User import UserSchema
 from app.services import PlaceService as placeService
 from app.utils import TypeUtilities as TypeUtilities
+from fastapi_pagination import Page, Params, paginate
 
 placeRouter = APIRouter(prefix="/place")
 
 #Get All Places
-@placeRouter.post("", response_description="Get list of places", response_model=PlaceSchema)
-def find_all(user: UserSchema, place: PlaceSchema):
-    response = TypeUtilities.parse_json(placeService.get_list_of_places(user, place))
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=response)
+@placeRouter.post("", 
+                  response_description="Get list of places", 
+                  response_model=Page[PlaceSchema]
+                  )
+def find_all(user: UserSchema, params: Params):
+    response = TypeUtilities.parse_json(placeService.get_list_of_places(user))
+    return paginate(response, params)
 
 
 @placeRouter.get("/{placeId}", response_description="Get place by Id", response_model=PlaceSchema)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import fastapi
 from app.routers import UserRouter as userRouter
 from app.routers import PlaceRouter as placerRouter
 from app.routers import MusicGenreRouter as musicGenreRouter
+from fastapi_pagination import add_pagination
 
 app = fastapi.FastAPI()
 
@@ -9,6 +10,8 @@ app = fastapi.FastAPI()
 app.include_router(userRouter.userRouter)
 app.include_router(placerRouter.placeRouter)
 app.include_router(musicGenreRouter.userRouter)
+
+add_pagination(app)
 
 
 

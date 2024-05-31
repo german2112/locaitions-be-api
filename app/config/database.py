0e5437dc29af4c19e2408952c60b6f85d31a5c43
german2112 <super.g.osi@hotmail.com>
2024-05-30 20:38:12 -0500

    chore: new database uri configuration

--- app/config/database.py
+++ app/config/database.py
@@ -2,7 +2,7 @@ import pymongo
 import sys
 
 try:
-    client = pymongo.MongoClient("mongodb+srv://german_torres:9tJmLztAXXxhZNPN@locaitionscluster1.3vimx9r.mongodb.net/?retryWrites=true&w=majority")
+    client = pymongo.MongoClient("mongodb+srv://loglobalproject:v0yzLDCHXgpDkGwp@locaitionscluster1.c7gk29f.mongodb.net/?retryWrites=true&w=majority&appName=locaitionscluster1") #TODO Find a way to hide the password from the uri
 except pymongo.errors.ConfigurationError:
   print("An Invalid URI host error was received. Is your Atlas host name correct in your connection string?")
   sys.exit(1)

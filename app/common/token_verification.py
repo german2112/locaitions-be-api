8f900d12b61dfe8a6cd10ff0d399d1bd02e7ead9
IanVelaz1 <37558466+IanVelaz1@users.noreply.github.com>
2024-06-03 13:06:28 -0500

    fix: fixed token verification taking too long (#38)

--- app/common/token_verification.py
+++ app/common/token_verification.py
@@ -1,14 +1,16 @@
 from fastapi import Depends, HTTPException, status, Request
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from firebase_admin import auth
+import asyncio
 
 security = HTTPBearer()
 
 
-def verify_firebase_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+async def verify_firebase_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
     token = credentials.credentials
     try:
-        decoded = auth.verify_id_token(id_token=token)
+        loop = asyncio.get_event_loop()
+        decoded = await loop.run_in_executor(None, auth.verify_id_token, token)
         return decoded
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,

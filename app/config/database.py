774b0f9cd0cda80cddb809db511fd1461c44a2db
german2112 <32340948+german2112@users.noreply.github.com>
2024-11-14 22:38:58 +0800

    Update database.py (#43)

--- app/config/database.py
+++ app/config/database.py
@@ -1,10 +1,1 @@
-import pymongo
-import sys
 
-try:
-    client = pymongo.MongoClient("mongodb+srv://main:TiKeWoOMtmBml4Yo@locaitionscluster1.3vimx9r.mongodb.net/?retryWrites=true&w=majority")
-except pymongo.errors.ConfigurationError:
-  print("An Invalid URI host error was received. Is your Atlas host name correct in your connection string?")
-  sys.exit(1)
-
-db = client.LOMNDB
